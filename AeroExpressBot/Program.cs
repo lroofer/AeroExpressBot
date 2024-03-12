a6891251e7fc70d9e957aac1f5127168b1c0b44d
Yegor Kolobaev <lroofer@icloud.com>
2024-03-12 11:35:50 +0300

    Initial commit

---
+++ AeroExpressBot/Program.cs
@@ -0,0 +1,51 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var botClient = new TelegramBotClient("6359168538:AAHbx-fnUR8BlOoTL1MzR9YVTTulDqF9c2w");
+
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for {me.Username}");
+Console.ReadLine();
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message) return;
+    if (message.Text is not { } messageText) return;
+    var chatId = message.Chat.Id;
+    Console.WriteLine($"Received a '{messageText}' message in char {chatId}'");
+    var sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: $"You said:\n{messageText}",
+        cancellationToken: cancellationToken);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var errorMessage = exception switch
+    {
+        ApiRequestException apiRequestException =>
+            $"Telegram API error:\n [{apiRequestException.ErrorCode}]\n[{apiRequestException.Message}]",
+        _ => exception.ToString()
+    };
+    Console.WriteLine(errorMessage);
+    return Task.CompletedTask;
+}
